5359ae833c102e8ebfc6374528f371ec3a1cc8f8
Aviv Ben Shahar <avivbens87@gmail.com>
2025-01-14 15:55:38 +0200

    fix: comment out config

--- apps/webpack-test/webpack.config.js
+++ apps/webpack-test/webpack.config.js
@@ -21,23 +21,23 @@ module.exports = composePlugins(withNx({}), (config) => {
 /* -------- Reload Issues ------------- */
 /* ------------------------------------ */
 
-module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/webpack-test'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
-    }),
-  ],
-};
+// module.exports = {
+//   output: {
+//     path: join(__dirname, '../../dist/apps/webpack-test'),
+//   },
+//   plugins: [
+//     new NxAppWebpackPlugin({
+//       target: 'node',
+//       compiler: 'tsc',
+//       main: './src/main.ts',
+//       tsConfig: './tsconfig.app.json',
+//       assets: ['./src/assets'],
+//       optimization: false,
+//       outputHashing: 'none',
+//       generatePackageJson: true,
+//     }),
+//   ],
+// };
 
 
 /* ------------------------------------ */
